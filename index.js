46e5533d84915b843c76ec983dde68ff6be9aa7b
Kritartha Bikram Shah <67311836+thisiskritartha@users.noreply.github.com>
2021-12-26 21:28:10 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+ console.log(req.body.cityName);
+ const query = req.body.cityName;
+ const apiKey = "#";
+ const unit = "metric";
+ const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit
+
+ https.get(url, function(response){
+   console.log(response.statusCode);
+   response.on("data", function(data){
+     const weatherData = JSON.parse(data)
+
+     const temp = weatherData.main.temp
+     const weatherDescription = weatherData.weather[0].description
+     const icon = weatherData.weather[0].icon
+     const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+     res.write("<h1>The temperature of " + query + " is " + temp + " Degree Celcius</h1>");
+     res.write("<p>The weather is currently " + weatherDescription + ".</p>");
+     res.write("<img src=" + imgURL + ">");
+     res.send();
+   })
+ })
+})
+
+app.listen(3000, function(){
+  console.log("Port 3000 is Working.");
+})
